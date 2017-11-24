f8f9628129bdd30dad87b39f154043cda9e22e04
Zhao Yulang <yulang.zhao@ge.com>
2017-11-24 15:13:16 +0800

    Install Notepad++

--- Notepad++/Install.bat
+++ Notepad++/Install.bat
@@ -2,5 +2,8 @@
 PUSHD %~dp0
 
 ECHO Installing Notepad++
+curl -k https://notepad-plus-plus.org/repository/7.x/7.5.1/npp.7.5.1.Installer.x64.exe -o npp.7.5.1.Installer.x64.exe
+
+npp.7.5.1.Installer.x64.exe /S
 
 POPD
