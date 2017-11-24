f872532a3b7c5ab09cf29085e51f3bb13a22b4fa
Zhao Yulang <yulang.zhao@ge.com>
2017-11-24 15:16:22 +0800

    Add some outputs during installing Notepad++

--- Notepad++/Install.bat
+++ Notepad++/Install.bat
@@ -1,9 +1,12 @@
 
 PUSHD %~dp0
 
-ECHO Installing Notepad++
+ECHO Downloading Notepad++
 curl -k https://notepad-plus-plus.org/repository/7.x/7.5.1/npp.7.5.1.Installer.x64.exe -o npp.7.5.1.Installer.x64.exe
 
+ECHO Installing Notepad++
 npp.7.5.1.Installer.x64.exe /S
+
+ECHO Notepad++ Installed
 
 POPD
