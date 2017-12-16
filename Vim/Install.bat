549844aef7622d614054cd625d82459108cb65c5
Zhao Yulang <yulang.zhao@ge.com>
2017-12-16 17:58:44 +0800

    Replace Pathgon to Vundle

--- Vim/Install.bat
+++ Vim/Install.bat
@@ -9,7 +9,7 @@ SET VIM_CONFIG_PATH=%USERPROFILE%\.vim
 RMDIR /S /Q %VIM_CONFIG_PATH%
 MKDIR %VIM_CONFIG_PATH%\bundle
 
-CALL :InstallPathogen
+git clone https://github.com/VundleVim/Vundle.vim.git %VIM_CONFIG_PATH%/bundle/Vundle.vim
 
 COPY _vimrc %USERPROFILE%
 
@@ -17,8 +17,3 @@ ENDLOCAL
 POPD
 GOTO :EOF
 
-:InstallPathogen
-git clone https://github.com/tpope/vim-pathogen.git
-ROBOCOPY vim-pathogen\autoload %VIM_CONFIG_PATH%\autoload
-RMDIR /S /Q vim-pathogen
-GOTO :EOF
