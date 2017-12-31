6e9c65e7838285236458a739558b2c9a516b5101
Zhao <212342455@logon.ds.ge.com>
2017-12-31 17:06:34 +0800

    Update Git version

--- Setup.bat
+++ Setup.bat
@@ -3,12 +3,12 @@ SETLOCAL
 PUSHD %~dp0
 
 ECHO Downloading Git for windows, please wait
-CALL :Download https://github.com/git-for-windows/git/releases/download/v2.15.0.windows.1/Git-2.15.0-64-bit.exe Git-2.15.0-64-bit.exe
+CALL :Download https://github.com/git-for-windows/git/releases/download/v2.15.1.windows.2/Git-2.15.1.2-64-bit.exe Git-2.15.1.2-64-bit.exe
 CALL :Download https://raw.githubusercontent.com/nnaabbcc/WinDev/master/Git/git_install.inf git_install.inf
 ECHO Git for windows downloaded
 
 ECHO Install Git
-Git-2.15.0-64-bit.exe /LOADINF="git_install.inf"
+Git-2.15.1.2-64-bit.exe /LOADINF="git_install.inf"
 
 ECHO Add Git to PATH
 SET PATH=C:\Program Files\Git\bin;%PATH%
