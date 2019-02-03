ddd57ca8a944de0cd1fb3d421e64f00782c690ce
nnaabbcc <nnaabbcc.zhao@gmail.com>
2019-02-03 13:29:48 +0800

    Install choco and use choco use some of the softwares

--- Install.bat
+++ Install.bat
@@ -28,4 +28,9 @@ EXIT /b
 :: Install TortoiseGit
 :: CALL :Install TortoiseGit
 
+:: Install Chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install 7zip activepresenter cmake dcmtk everything filezilla meld python3 teamviewer vscode you-get grepwin foxitreader
+
 EXIT /b
